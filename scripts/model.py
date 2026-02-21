a7dd7386ede0c9d09c337aff07c96bf6df467876
Danny <dabfisher@gmail.com>
2026-02-21 00:05:29 -0800

    updated model to prep data from ingest historical for utilization and a note that the prior push included temp_soil_historical.csv which will be stored in the repo to prevent calls

--- scripts/model.py
+++ scripts/model.py
@@ -6,10 +6,16 @@ import pandas as pd
 # Connect to a local database file
 con = duckdb.connect("data/weather.db")
 
-# Load the raw csv into a table 
+# Load the raw forecast csv into a table 
 con.execute("""
     CREATE OR REPLACE TABLE raw_weather AS
     SELECT * FROM read_csv_auto('data/weather_raw.csv')
+""")
+
+# Load the raw historical csv into a table 
+con.execute("""
+    CREATE OR REPLACE TABLE temp_soil_historical AS
+    SELECT * FROM read_csv_auto('data/temp_soil_historical.csv')
 """)
 
 # Determine when temperature dips into plants that can be planted
@@ -72,8 +78,53 @@ con.execute("""
         ORDER BY city, week_start
 """)
 
+# Establish historical metrics needed for last frost
+con.execute("""
+    CREATE OR REPLACE TABLE avg_last_freeze_date AS
+    WITH max_freeze_date AS (
+        SELECT 
+            city,
+            YEAR(date) AS year,
+            YEAR(current_date) AS current_year,
+            MAX(date) AS last_freeze
+        FROM temp_soil_historical
+        WHERE
+            MONTH(date) <= 6
+            AND temp_min <= 32
+        GROUP BY 
+            city,
+            YEAR(date),
+            YEAR(current_date)
+        )
+        SELECT
+            city,
+            MAKE_DATE(ANY_VALUE(current_year), 1, 1) + CAST(AVG(DAYOFYEAR(last_freeze)) AS INTEGER) AS avg_last_freeze_all_time,
+            MAKE_DATE(ANY_VALUE(current_year), 1, 1) + CAST(AVG(CASE WHEN year >= current_year - 10 THEN DAYOFYEAR(last_freeze) END) AS INTEGER) AS avg_last_freeze_ten_years,
+            MAKE_DATE(ANY_VALUE(current_year), 1, 1) + CAST(AVG(CASE WHEN year >= current_year - 5 THEN DAYOFYEAR(last_freeze) END) AS INTEGER) AS avg_last_freeze_five_years
+        FROM max_freeze_date
+        GROUP BY city
+        ORDER BY city
+""")
+
+# Establish historical metrics for daily average soil temps
+con.execute("""
+    CREATE OR REPLACE TABLE avg_soil_temp_daily AS
+        SELECT
+            city,
+            MAKE_DATE(YEAR(current_date), 1, 1) + CAST(DAYOFYEAR(date) AS INTEGER) AS date,
+            AVG(soil_temp_0_7cm) AS avg_shallow_soil_temp,
+            AVG(soil_temp_7_to_28cm) AS avg_deep_soil_temp
+        FROM temp_soil_historical
+        GROUP BY city,MAKE_DATE(YEAR(current_date), 1, 1) + CAST(DAYOFYEAR(date) AS INTEGER) 
+        ORDER BY city,date
+""")
+
 result = con.execute("""
-    SELECT * FROM irrigation_tracker
-    WHERE city = 'Portland'
+    SELECT * FROM avg_last_freeze_date
+""").df()
+print(result)
+
+result = con.execute("""
+    SELECT * FROM avg_soil_temp_daily
 """).df()
 print(result)
